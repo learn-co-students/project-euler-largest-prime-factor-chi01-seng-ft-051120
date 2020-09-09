7168e7791e28fdc7526cac89ebcaa193d8d2bbbd
jequezinho <jroverby92@gmail.com>
2020-09-09 10:51:42 -0500

    Done.

--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,18 @@
-# Enter your object-oriented solution here!
+class LargestPrimeFactor
+
+    def initialize(n)
+        @n = n
+    end
+
+    def number(n = @n)
+        ceil = (n ** 0.5).floor()
+        i = 2
+    
+        while (n % i != 0) && (i <= ceil)
+            i += 1
+        end
+    
+        i > ceil ? n : self.number(n / i)
+    end
+
+end
